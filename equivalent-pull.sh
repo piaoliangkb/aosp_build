9325106e57d121de61d4eeca4f37096646eae128
piaoliangkb <piaoliangkb@gmail.com>
2023-10-14 21:59:57 +0800

    up

---
+++ equivalent-pull.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/opengapps/aosp_build.git -b master vendor/opengapps/build
+git clone --depth 1 https://gitlab.opengapps.org/opengapps/all.git -b master vendor/opengapps/sources/all
+git clone --depth 1 https://gitlab.opengapps.org/opengapps/arm.git -b master vendor/opengapps/sources/arm
+git clone --depth 1 https://gitlab.opengapps.org/opengapps/arm64.git -b master vendor/opengapps/sources/arm64
